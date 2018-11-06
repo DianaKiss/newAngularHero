05e54524bb531cb5c5b22a142bb84dd523602bb9
DianaKiss <dana.butcher@gmail.com>
2018-11-06 11:51:27 +0100

    Adding routing to new components

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  { path: 'detail/:id', component: HeroDetailComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: 'posts', component: PostsComponent }
 ];
 
 @NgModule({
@@ -22,6 +28,6 @@ const routes: Routes = [
   ],
 
 })
-export class AppRoutingModule { 
+export class AppRoutingModule {
 
 }
